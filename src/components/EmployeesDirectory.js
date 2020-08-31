21fbe584ac8eacc91913a5ffb32012df2f08b075
Mark <mark.litfin@yahoo.com>
2020-08-31 14:39:32 -0400

    added the components, css, and API

---
+++ src/components/EmployeesDirectory.js
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+import Header from './Header';
+import Search from './Search';
+import EmployeesTable from './EmployeesTable';
+import API from "../utils/Api";
+
+class EmployeesDirectory extends Component {
+    //set the state
+    state = {
+        employees: [],
+        query: '',
+        sortedEmployees: [],
+        sortType: ''
+    };
+
+    componentDidMount() {
+        this.loadAllEmployees();
+    }
+
+    loadAllEmployees() {
+        API.getEmployees()
+        .then(res => { 
+            this.createNewEmployeesList(res.data.results);
+        })
+        .catch(err => console.log(err));
+    }
+
+    createNewEmployeesList = employees => {
+
+        const newEmployeesList = employees.map((employee => {
+
+            return (
+                {
+                    dob: employee.dob.date,
+                    email: employee.email,
+                    name: `${employee.name.first} ${employee.name.last}`,
+                    phone: employee.phone,
+                    image: employee.picture.large
+                }
+            )
+
+        }))
+
+        this.setState({
+            employees: newEmployeesList
+        })
+    }
+
+     //Filter by search input:
+    filterByName = event => {
+
+        const query = event.target.value;
+
+        this.setState({
+            query
+        }, () => {
+
+            let employeesList;
+
+            if (this.state.sortedEmployees.length > 0) {
+                employeesList = this.state.sortedEmployees;
+            } else {
+                employeesList = this.state.employees;
+            }
+            //add display: true/false to display the record if it matches the search string:
+            const newEmployeeTable = employeesList.map(employee => {
+                let name = employee.name.toLowerCase();
+
+                if (name.indexOf(this.state.query.toLowerCase()) !== -1) {
+                    return {...employee, display: true}
+                } else {
+                    return {...employee, display: false}
+                }
+            });
+
+            this.setState({
+                employees: newEmployeeTable,
+                sortedEmployees: newEmployeeTable
+            });
+        });
+    }
+
+    //sort alphabetically/reverse alphabetically employees by some name key:
+    sortAlpha = employees => {
+
+        const sortType = this.state.sortType;
+
+        if (sortType === 'desc' || sortType === '') {
+            //sort ascending order:
+            employees.sort(this.dynamicSort("name"));
+            this.setState({
+                sortedEmployees: employees,
+                sortType: 'asc'
+            });
+        } else if (sortType === 'asc') {
+            //sort descending order:
+            employees.sort(this.dynamicSort("-name"));
+            this.setState({
+                sortedEmployees: employees,
+                sortType: 'desc'
+            });
+        }
+
+    }
+
+    //reorders employees list alphabetically/reverse alphabetically:
+    dynamicSort = property => {
+
+        var sortOrder = 1;
+
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+
+        return function (a,b) {
+            if (sortOrder === -1) {
+                return b[property].localeCompare(a[property]);
+            } else {
+                return a[property].localeCompare(b[property]);
+            }        
+        }
+    }
+
+    render() {
+
+        let employeesList;
+
+        if (this.state.sortedEmployees.length > 0) {
+            employeesList = this.state.sortedEmployees;
+        } else {
+            employeesList = this.state.employees;
+        }
+        
+        let sortArrow;
+
+        const sortState = this.state.sortType;
+
+        if (sortState === 'asc') {
+            //down arrow
+            sortArrow = <span>&#9660;</span>;
+        } else if (sortState === 'desc') {
+            //up arrow
+            sortArrow = <span>&#9650;</span>;
+        } else {
+            sortArrow = '';
+        }
+
+        return(
+            <>
+                <Header />
+                <Search filterByName={this.filterByName} query={this.state.query} />
+                <EmployeesTable employees={employeesList} sortAlpha={this.sortAlpha} sortArrow={sortArrow}  />
+            </>
+        );
+    }
+}
+
+export default EmployeesDirectory;
---
+++ src/components/EmployeesTable.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import Employee from './Employee';
+
+class EmployeesTable extends Component {
+
+    renderEmployee = (employee, index) => {
+
+        let displayed;
+
+        if (employee.display === undefined) {
+            displayed = true;
+        } else {
+            displayed = employee.display;
+        }
+
+        if (displayed === true) {
+            return (
+                <Employee 
+                    key={index}
+                    order={index + 1}
+                    image={employee.image}
+                    name={employee.name}
+                    phone={employee.phone}
+                    email={employee.email}
+                    dob={employee.dob}
+                />
+            );
+        }
+    }
+
+    render(props) {
+        
+        return (
+            <table className="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col" className="sort" title="Sort alphabetically" 
+                            onClick={() => this.props.sortAlpha(this.props.employees)}>Name {this.props.sortArrow}</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">DOB</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.employees.map((employee, index) => (
+                            this.renderEmployee(employee, index)
+                        ))
+                    }
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default EmployeesTable;
